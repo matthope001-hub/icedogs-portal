038182ec2b0f8454ece105020829dbc456db5fc3
matthope001-hub <matt.hope001@gmail.com>
2026-07-31 14:10:03 -0400

    Clean up headers in fetch request and response

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ export default {
     const url = new URL(request.url);
     const target = APPS_SCRIPT_URL + url.search;
 
+    const headers = new Headers(request.headers);
+    headers.delete("host");
+    headers.delete("cf-connecting-ip");
+    headers.delete("cf-ray");
+    headers.delete("cf-visitor");
+
     const init = {
       method: request.method,
-      headers: request.headers,
+      headers,
       redirect: "follow"
     };
     if (request.method !== "GET" && request.method !== "HEAD") {
@@ -17,6 +23,8 @@ export default {
     const newHeaders = new Headers(res.headers);
     newHeaders.delete("content-security-policy");
     newHeaders.delete("x-frame-options");
+    newHeaders.delete("content-encoding");
+    newHeaders.delete("content-length");
 
     return new Response(res.body, { status: res.status, headers: newHeaders });
   }
