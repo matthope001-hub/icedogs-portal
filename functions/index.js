983a20441eb66429f4e85109406463e7c8af2365
matthope001-hub <matt.hope001@gmail.com>
2026-07-31 14:15:09 -0400

    Refactor onRequest to default export fetch function

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,31 @@
-export async function onRequest(context) {
-  const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwaTRUEmRVQ4P5zUpOccWkmQa_1V4yM7DxwSCjfRo73ht2mrpYW-oH8gt38L5mmLfaj9A/exec";
-  const url = new URL(context.request.url);
-  const target = APPS_SCRIPT_URL + url.search;
+export default {
+  async fetch(request) {
+    const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwaTRUEmRVQ4P5zUpOccWkmQa_1V4yM7DxwSCjfRo73ht2mrpYW-oH8gt38L5mmLfaj9A/exec";
+    const url = new URL(request.url);
+    const target = APPS_SCRIPT_URL + url.search;
 
-  const init = {
-    method: context.request.method,
-    headers: context.request.headers,
-    redirect: "follow"
-  };
-  if (context.request.method !== "GET" && context.request.method !== "HEAD") {
-    init.body = await context.request.arrayBuffer();
+    const headers = new Headers(request.headers);
+    headers.delete("host");
+    headers.delete("cf-connecting-ip");
+    headers.delete("cf-ray");
+    headers.delete("cf-visitor");
+
+    const init = {
+      method: request.method,
+      headers,
+      redirect: "follow"
+    };
+    if (request.method !== "GET" && request.method !== "HEAD") {
+      init.body = await request.arrayBuffer();
+    }
+
+    const res = await fetch(target, init);
+    const newHeaders = new Headers(res.headers);
+    newHeaders.delete("content-security-policy");
+    newHeaders.delete("x-frame-options");
+    newHeaders.delete("content-encoding");
+    newHeaders.delete("content-length");
+
+    return new Response(res.body, { status: res.status, headers: newHeaders });
   }
-
-  const res = await fetch(target, init);
-  const newHeaders = new Headers(res.headers);
-  newHeaders.delete("content-security-policy");
-  newHeaders.delete("x-frame-options");
-
-  return new Response(res.body, { status: res.status, headers: newHeaders });
-}
+};
